38dea371d7ab9dd4b367173b77ba5d852cb91d03
Vatsal Patel <vp7364964@gmail.com>
2026-04-24 11:19:44 +0530

    sameSite cookies set none

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -59,14 +59,14 @@ const sendTokens = (
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "strict",
     maxAge: 15 * 60 * 1000,
   });
 };
